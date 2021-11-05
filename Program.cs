9b27f295495b85358a7ec4cbb8b2d46ab3ad99e0
JimBeakIt <87563663+JimBeakIt@users.noreply.github.com>
2021-11-05 09:55:35 +0000

    update

--- Program.cs
+++ Program.cs
@@ -10,6 +10,23 @@ namespace LoopAssignment
     {
         static void Main(string[] args)
         {
+            //int i = 20;
+            //while (i <= 30)
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+            //}
+            //Console.ReadLine();
+
+            int i = 9;
+
+            do
+            {
+                Console.WriteLine("i = {0}", i);
+                i++;
+
+            } while (i < 60);
+            Console.ReadLine();
         }
     }
 }
